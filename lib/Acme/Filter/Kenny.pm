6e87dd32f396d247ade832a7ade6bb43fc34fc08
Gergely Nagy <algernon@cpan.org>
2004-05-14 10:38:30 +0000

    Import of ALGERNON/Acme-Filter-Kenny-1.01 from CPAN.
    gitpan-cpan-distribution: Acme-Filter-Kenny
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         ALGERNON/Acme-Filter-Kenny-1.01.tar.gz
    gitpan-cpan-author:       ALGERNON
    gitpan-cpan-maturity:     released

--- lib/Acme/Filter/Kenny.pm
+++ lib/Acme/Filter/Kenny.pm
@@ -23,9 +23,10 @@ package Acme::Filter::Kenny;
 use strict;
 use warnings;
 use Filter::Util::Call;
-use vars qw($VERSION);
+use vars qw($VERSION $iq_kenny);
 
-$VERSION = "1.00";
+$VERSION = "1.01";
+$iq_kenny = 0;
 
 sub KennyIt {
    ($_)=@_;my($p,$f);$p=3-2*/[^\W\dmpf_]/i;s.[a-z]{$p}.vec($f=join('',$p-1?chr(
@@ -34,15 +35,24 @@ sub KennyIt {
 };
 
 sub import {
-	my ($type) = @_;
+	my ($type, @params) = @_;
 	my ($ref) = [];
+
+	if (grep {/^:iq_kenny$/} @params) {
+		$iq_kenny = 1;
+	}
 	filter_add (bless $ref);
 }
 
 sub filter {
 	my $self = (@_);
 	my ($status);
-	KennyIt ($_) if ($status = filter_read ()) > 0;
+	if (($status = filter_read ()) > 0) {
+		if ($iq_kenny == 0 ||
+		    m/^[^a-z]*[mfp]{3}(?:[^a-z]|[mfp]{3})+$/i) {
+			KennyIt ($_);
+		}
+	}
 	$status;
 }
 
@@ -60,17 +70,31 @@ Acme::Filter::Kenny - Kenny source filter
 
   use Acme::Filter::Kenny;
 
+Or
+
+  use Acme::Filter::Kenny qw/:iq_kenny/;
+
 =head1 DESCRIPTION
 
 This source filter translates code in Kenny-speak back to normal perl
-before actually parsing the rest of the stream.
+before actually parsing the rest of the stream. When used with the
+B<:iq_kenny> tag, Acme::Filter::Kenny will translate only those lines
+that look like Kenny-speak, not all of them.
+
+Be aware that B<:iq_kenny> applies heuristics, it might get some
+things wrong, so use with care!
 
 =head1 EXAMPLE
 
   use Acme::Filter::Kenny;
   pfmpffmffpppfmp "Mfpmpppmfpmfppf fppppfpffpmfmpm!\ppp";
 
-This will print "Hello World!".
+This will print "Hello World!", and so will this:
+
+  use Acme::Filter::Kenny qw/:iq_kenny/;
+  print <<EOF
+  Mfpmpppmfpmfppf fppppfpffpmfmpm!
+  EOF
 
 =head1 AUTHOR
 
